816e948c7e467127aca422179d205ff525487730
Your Name <you@example.com>
2016-03-17 14:52:40 +0800

    first commit

---
+++ FlycoRoundView/gen/com/flyco/RoundViewSample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.flyco.RoundViewSample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ FlycoTabLayout-Demo/gen/com/flyco/tablayoutsamples/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.flyco.tablayoutsamples;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
